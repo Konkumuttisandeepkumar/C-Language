f9b3e9d83756d35a06eb2503e78d9fb6b3c0dc95
Konkumuttisandeepkumar <94606423+Konkumuttisandeepkumar@users.noreply.github.com>
2024-09-24 17:05:21 +0530

    Add files via upload

---
+++ 6.Control Statements/3.Jump Statements/1.break_Statement.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int i=0;
+
+for(i=0;i<10;i++)
+{
+if(i==7)
+{
+    	break;
+}
+	printf("%d\n",i);
+}
+return 0;
+}
+
---
+++ 6.Control Statements/3.Jump Statements/2.continue_Statement.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int i=0;
+
+for(i=0;i<10;i++)
+{
+if(i%2==0)
+{
+        continue;
+}
+        printf("%d\n",i);
+}
+return 0;
+}
